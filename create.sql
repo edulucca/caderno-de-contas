2212cf809d32b5ed8b7fba5e711dda9d22114c07
Eduardo De Lucca <eduardo.luccati@gmail.com>
2023-04-08 22:28:58 -0300

    CConfiguração - Zipkin

--- create.sql
+++ create.sql
@@ -46,3 +46,6 @@ alter table divida add constraint FKhw5grl4lp6gp8j3u6dwlbde7 foreign key (divida
 create table divida (id integer not null auto_increment, data_de_pgto date, data_final date, descricao varchar(255), nome varchar(255), tipo_divida varchar(255), valor float(53) not null, divida_mes_id integer, primary key (id)) engine=MyISAM;
 create table divida_mes (id integer not null auto_increment, num_mes integer, primary key (id)) engine=MyISAM;
 alter table divida add constraint FKhw5grl4lp6gp8j3u6dwlbde7 foreign key (divida_mes_id) references divida_mes (id);
+create table divida (id integer not null auto_increment, data_de_pgto date, data_final date, descricao varchar(255), nome varchar(255), tipo_divida varchar(255), valor float(53) not null, divida_mes_id integer, primary key (id)) engine=InnoDB;
+create table divida_mes (id integer not null auto_increment, num_mes integer, primary key (id)) engine=InnoDB;
+alter table divida add constraint FKhw5grl4lp6gp8j3u6dwlbde7 foreign key (divida_mes_id) references divida_mes (id);
